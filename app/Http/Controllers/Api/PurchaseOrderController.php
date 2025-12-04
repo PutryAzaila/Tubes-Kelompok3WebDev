74597b1bf76768e242744a791d5671938daa5d99
DavidAndreasMarpaung <davidandreasmarpaung333@gmail.com>
2025-12-04 15:55:36 +0700

    Purchase Order

--- app/Http/Controllers/Api/PurchaseOrderController.php
+++ app/Http/Controllers/Api/PurchaseOrderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Pocontroller extends Controller
+class PurchaseOrderController extends Controller
 {
     //
 }
