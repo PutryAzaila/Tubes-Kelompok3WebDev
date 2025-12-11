881af1a9f9a819597ecc283b9162ca6367aedac5
PutryAzaila <putryazaila1306@gmail.com>
2025-12-11 15:19:26 +0700

    fix bug

--- app/Http/Controllers/Api/PurchaseOrderController.php
+++ app/Http/Controllers/Api/PurchaseOrderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PurchaseOrder extends Controller
+class PurchaseOrderController extends Controller
 {
     //
 }
