8d0e83b7341e52d6670678068380cdd6e114f97d
DavidAndreasMarpaung <davidandreasmarpaung333@gmail.com>
2025-12-18 19:32:56 +0700

    update web route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\KategoriVendorController;
 use App\Http\Controllers\Api\DataVendorController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\API\PurchaseOrderController;
+use App\Http\Controllers\API\DetailPurchaseOrderController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -37,4 +39,36 @@ Route::prefix('vendor')->group(function () {
     Route::put('/{id}', [DataVendorController::class, 'update']);
     Route::delete('/{id}', [DataVendorController::class, 'destroy']);
     Route::get('/kategori/{kategoriId}', [DataVendorController::class, 'getByCategory']);
+});
+
+// Purchase Order Routes
+Route::prefix('purchase-orders')->group(function () {
+    // CRUD Routes
+    Route::get('/', [PurchaseOrderController::class, 'index']);
+    Route::post('/', [PurchaseOrderController::class, 'store']);
+    Route::get('/{id}', [PurchaseOrderController::class, 'show']);
+    Route::get('/{id}/edit', [PurchaseOrderController::class, 'edit']);
+    Route::put('/{id}', [PurchaseOrderController::class, 'update']);
+    Route::delete('/{id}', [PurchaseOrderController::class, 'destroy']);
+    
+    // Status Management Routes
+    Route::patch('/{id}/approve', [PurchaseOrderController::class, 'approveStatus']);
+    Route::patch('/{id}/reject', [PurchaseOrderController::class, 'rejectStatus']);
+    
+    // Dashboard Stats
+    Route::get('/stats/dashboard', [PurchaseOrderController::class, 'getDashboardStats']);
+});
+
+// Detail Purchase Order Routes
+Route::prefix('detail-purchase-orders')->group(function () {
+    // Get details by PO ID
+    Route::get('/by-po/{id_po}', [DetailPurchaseOrderController::class, 'getByPurchaseOrder']);
+    
+    // CRUD for items
+    Route::post('/', [DetailPurchaseOrderController::class, 'store']);
+    Route::put('/{id}', [DetailPurchaseOrderController::class, 'update']);
+    Route::delete('/{id}', [DetailPurchaseOrderController::class, 'destroy']);
+    
+    // Check stock
+    Route::get('/check-stock/{id_perangkat}', [DetailPurchaseOrderController::class, 'checkStock']);
 });
